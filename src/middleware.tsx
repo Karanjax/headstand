0c9a9a664824da432a7a93cdc69f609b5ed5344f
Karanjax <karanoreika@gmail.com>
2024-12-09 18:09:24 -0500

    Update middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,23 +1,23 @@
 import { withAuth } from "next-auth/middleware";
 import { pagesOptions } from "./app/api/auth/[...nextauth]/pages-options";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export default withAuth(
-  function middleware(req) {
-    const { pathname } = req.nextUrl;
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
 
-    // Allow public access to API route
-    if (pathname.startsWith("/api/instructor")) {
-      return NextResponse.next();
-    }
-  },
-  {
+  // Allow public access to API route
+  if (pathname.startsWith("/api/instructor")) {
+    return NextResponse.next(); // Skip authentication
+  }
+
+  // Apply authentication for all other routes
+  return withAuth(req, {
     pages: {
       ...pagesOptions,
     },
-  }
-);
+  });
+}
 
 export const config = {
-  matcher: ["/:path*"], // Protect all routes except API exclusions
+  matcher: ["/:path*"], // Protect all routes except `/api/instructor`
 };
