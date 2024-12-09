ae43fe560bf5d38d310174e9a653359bbfdf3ace
Karanjax <karanoreika@gmail.com>
2024-12-09 11:09:39 -0500

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,11 @@ module.exports = {
     "node/no-missing-import": "off",
     "react-hooks/exhaustive-deps": "off",
     "react/no-unescaped-entities": "off",
+    "@next/next/no-img-element": "off",
+    "react/display-name": "off",
+    "jsx-a11y/alt-text": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "react/jsx-key": "off",
+    "@next/next/no-sync-scripts": "off",
   },
 };
