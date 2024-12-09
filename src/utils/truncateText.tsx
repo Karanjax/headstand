2552f3733208d61778d4fa5264b227664409898d
Karanjax <karanoreika@gmail.com>
2024-12-09 11:21:40 -0500

    Update truncate text

--- src/utils/truncateText.tsx
+++ src/utils/truncateText.tsx
@@ -1,4 +1,4 @@
-export function truncateText(text, length = 100) {
+export function truncateText(text: string, length: number = 100): string {
   if (!text) return "";
   return text.length > length ? text.substring(0, length) + "..." : text;
 }
